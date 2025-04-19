e6ee5921e33afd5baabd5b816bfa48705ff8dca8
root <10484@holbertonstudents.com>
2025-04-19 12:06:24 +0000

    feat: add 1st task

--- test/shell_tutorial/shell.c
+++ test/shell_tutorial/shell.c
@@ -1,50 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <sys/wait.h>
 
 int main(void)
 {
-	size_t buf_size = 0;
-	char *buf = NULL;
-	char *token;
-	int status, i = 0;
-	char **array;
-	pid_t child_pid;
+    char *buf = NULL;
+    size_t buf_size = 0;
+    ssize_t nread;
+    pid_t pid;
+    int status;
+    char *argv[2];
 
+    while (1)
+    {
+        write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-	while (1)
-	{
-		write(1, "#cisfun$ ", 9);
-		getline(&buf, &buf_size, stdin);
-		token = strtok(buf, "\t\n");
-		array = malloc(sizeof(char *) * 1024);
+        nread = getline(&buf, &buf_size, stdin);
+        if (nread == -1)
+        {
+            write(STDOUT_FILENO, "\n", 1);  /*For clean exit on Ctrl+D*/
+            break;
+        }
 
-		while (token)
-		{
-			array[i] = token;
-			token = strtok(NULL, "\t\n");
-			i++;
-		}
-		array[i] = NULL;
-		child_pid = fork();
+        // Remove trailing newline
+        if (buf[nread - 1] == '\n')
+            buf[nread - 1] = '\0';
 
-		if (child_pid == -1)
-		{
-			perror("Error");
-			return (1);
-		}
-		if (child_pid == 0)
-		{
-			if (execve(array[0], array, NULL) == -1)
-				perror("Error with the Exec");
-		}
-		else
-		{
-			wait(&status);
-		}
-		free(array);
-	}
+        if (buf[0] == '\0')
+            continue;
+
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("fork");
+            continue;
+        }
+
+        if (pid == 0)
+        {
+            argv[0] = buf;
+            argv[1] = NULL;
+            execve(buf, argv, NULL);
+            perror("./shell");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(&status);
+        }
+    }
+    free(buf);
 }
